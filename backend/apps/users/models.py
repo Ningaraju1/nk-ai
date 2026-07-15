c70f4d3851d232de27a7797aa0f3fff5e0b40786
Ningaraju1 <ningarajappu147@gmail.com>
2026-07-15 17:10:34 +0530

    feat(users): implement user manager

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from .managers import UserManager
 
-# Create your models here.
+class User(AbstractUser):
+    """
+    Custom User model.
+    """
+
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN", "Admin"
+        USER = "USER", "User"
+        SUPPORT = "SUPPORT", "Support"
+
+    email = models.EmailField(
+        unique=True,
+    )
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.USER,
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
+
+
+    objects = UserManager()
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
